915697823dfd6d3af1104c21d1e0f54f0acad6e9
Marc Dwyer <marcdwyer1738@gmail.com>
2024-05-21 22:35:52 -0500

    updated comments;

--- background/main.ts
+++ background/main.ts
@@ -3,11 +3,11 @@ import { ASCIITable } from "../shared/ASCIITable";
 import { checkIfValidBinary } from "../shared/checkIfValidBinary";
 
 function binaryToText(binaryText: string): string {
-  // Parse the binary byte as an integer with base 2
   let text = "";
   const binaryList = binaryText.split(" ");
 
   for (const binary of binaryList) {
+    // Parse the binary byte as an integer with base 2
     const decimal = parseInt(binary, 2);
     text += ASCIITable.get(decimal);
   }
